a699ff04cb486caa7c9b0361bb5e3329a4aa085c
Avtex19 <atukaberadze19@gmail.com>
2024-09-06 01:49:17 +0400

    created views and corresponding urls

--- myproject/myapp/api/urls.py
+++ myproject/myapp/api/urls.py
@@ -1,9 +1,19 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
-from .views import Home
+from .views import *
+
 
 urlpatterns = [
     path('', views.getRoutes),
     path('home/', Home.as_view(), name='home'),
+    re_path(r'^users/$', UserList.as_view()),
+    re_path(r'^create-users/$', UserCreate.as_view()),
+    re_path(r'^genres/$', GenreListCreate.as_view()),
+    re_path(r'^genres/(?P<pk>\d+)/$', GenreRetrieveUpdate.as_view()),
 
+    re_path(r'^movies/$', MovieListCreate.as_view()),
+    re_path(r'^movies/(?P<pk>\d+)/$', MovieRetrieveUpdate.as_view()),
+
+    re_path(r'^comments/$', CommentListCreate.as_view()),
+    re_path(r'^comments/(?P<pk>\d+)/$', CommentRetrieveUpdate.as_view())
 ]
--- myproject/myapp/api/views.py
+++ myproject/myapp/api/views.py
@@ -3,6 +3,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from ..models import Genre, Movie, Comment
+from .serializers import UserSerializer, GenreSerializer, MovieSerializer, CommentSerializer
 
 
 @api_view(['GET'])
@@ -20,3 +24,67 @@ class Home(APIView):
     def get(self, request):
         content = {'message': 'Hello, World!'}
         return Response(content)
+
+
+class UserList(generics.ListAPIView):
+    """View to list all users"""
+    queryset = User.objects.all().order_by('first_name')
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class UserCreate(generics.CreateAPIView):
+    """View to create a new user"""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class UserRetrieveUpdate(generics.RetrieveUpdateAPIView):
+    """ Retrieve a user or update user information.
+        Accepts GET and PUT requests and the record id must be provided in the request """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class GenreListCreate(generics.ListCreateAPIView):
+    """View to create a new genre"""
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class GenreRetrieveUpdate(generics.RetrieveUpdateAPIView):
+    """ Retrieve and update Genre information """
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+
+class MovieListCreate(generics.ListCreateAPIView):
+    """List and create movies """
+    queryset = Movie.objects.all().order_by('name')
+    serializer_class = MovieSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+
+class MovieRetrieveUpdate(generics.RetrieveUpdateAPIView):
+    """Retrieve and update a movie"""
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+
+class CommentListCreate(generics.ListCreateAPIView):
+    """ List or create a movie """
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+
+class CommentRetrieveUpdate(generics.RetrieveUpdateAPIView):
+    """ List or create a movie """
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = (permissions.IsAuthenticated,)
